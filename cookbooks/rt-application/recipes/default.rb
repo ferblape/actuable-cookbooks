80b31261ac98ffc42df4f739d568372ad58be9a6
Fernando Blat <ferblape@gmail.com>
2012-02-02 17:41:25 -0300

    Deploy and reboot RT app

--- cookbooks/rt-application/recipes/default.rb
+++ cookbooks/rt-application/recipes/default.rb
@@ -14,3 +14,16 @@ end
 execute "update the code" do
   command "cd /var/www/actuable-rt; git pull"
 end
+
+cookbook_file "server script" do
+  path "/var/www/actuable-rt/server.sh"
+  source "server.sh"
+  owner "www-data"
+  group "www-data"
+  mode 0755
+  not_if { Dir.exists?("/var/www/actuable-rt/server.sh") }
+end
+
+execute "reboot the server" do
+  command "/var/www/actuable-rt/server.sh"
+end
