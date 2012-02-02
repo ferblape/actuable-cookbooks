ddaf754cd4416b93a5369f0783bc884b0028d557
Fernando Blat <ferblape@gmail.com>
2012-02-02 17:14:55 -0300

    Directory /var/www

--- cookbooks/application/recipes/default.rb
+++ cookbooks/application/recipes/default.rb
@@ -1,5 +1,13 @@
 include_recipe 'cron'
 
+directory "/var/www" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode "0750"
+  action :create
+  not_if { Dir.exists?("/var/www") }
+end
+
 directory "/var/www/actuable" do
   owner "ubuntu"
   group "ubuntu"
