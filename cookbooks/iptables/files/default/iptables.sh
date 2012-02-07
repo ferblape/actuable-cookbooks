8dccdf5a18c1531efa911c45b049a3e4e87c2278
Fernando Blat <ferblape@gmail.com>
2012-02-06 21:51:20 -0300

    Firewall blocks NFS, RCPBidn, Redis, Mysql and Memcached from IP's not from Actuable

--- cookbooks/iptables/files/default/iptables.sh
+++ cookbooks/iptables/files/default/iptables.sh
@@ -11,18 +11,51 @@ iptables -X PACKET_DROPS
 
 iptables -N PACKET_DROPS
 
-### ACEPTA TODO EL TRAFICO DE CONEXIONES YA ESTABLECIDAS O RELACIONADAS
+# Accept everything unless what is blocked
 iptables -A INPUT -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Se descartan las conexiones al puerto 25 desde internet
+# smtp
 iptables -A INPUT -p tcp -i eth0 --destination-port 25 -j REJECT
 
-# Se descartan las conexiones al puerto 5432 desde internet
+# 5432 port
 iptables -A INPUT -p tcp -i eth0 --destination-port 5432 -j PACKET_DROPS
 
-# Se descartan las conexiones al SNMP desde internet
+# snmp
 iptables -A INPUT -p tcp -i eth0 --destination-port 199 -j PACKET_DROPS
 iptables -A INPUT -p udp -i eth0 --destination-port 161 -j PACKET_DROPS
 
-# Se descartan las conexiones al SNMP desde internet
-iptables -A INPUT -p tcp -i eth0 --destination-port 6379 -j PACKET_DROPS
+# Redis
+iptables -A INPUT -s 192.168.152.20 -p tcp --destination-port 6379 -j ACCEPT
+iptables -A INPUT -s 192.168.152.171 -p tcp --destination-port 6379 -j ACCEPT
+iptables -A INPUT -s 192.168.176.63 -p tcp --destination-port 6379 -j ACCEPT
+iptables -A INPUT -s 192.168.186.70 -p tcp --destination-port 6379 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 6379 -j REJECT
+
+# Mysql
+iptables -A INPUT -s 192.168.152.20 -p tcp --destination-port 3306 -j ACCEPT
+iptables -A INPUT -s 192.168.152.171 -p tcp --destination-port 3306 -j ACCEPT
+iptables -A INPUT -s 192.168.176.63 -p tcp --destination-port 3306 -j ACCEPT
+iptables -A INPUT -s 192.168.186.70 -p tcp --destination-port 3306 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 3306 -j REJECT
+
+# Memcached
+iptables -A INPUT -s 192.168.152.20 -p tcp --destination-port 11211 -j ACCEPT
+iptables -A INPUT -s 192.168.152.171 -p tcp --destination-port 11211 -j ACCEPT
+iptables -A INPUT -s 192.168.176.63 -p tcp --destination-port 11211 -j ACCEPT
+iptables -A INPUT -s 192.168.186.70 -p tcp --destination-port 11211 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 11211 -j REJECT
+
+# NFS
+iptables -A INPUT -s 192.168.152.20 -p tcp --destination-port 2049 -j ACCEPT
+iptables -A INPUT -s 192.168.152.171 -p tcp --destination-port 2049 -j ACCEPT
+iptables -A INPUT -s 192.168.176.63 -p tcp --destination-port 2049 -j ACCEPT
+iptables -A INPUT -s 192.168.186.70 -p tcp --destination-port 2049 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 2049 -j REJECT
+
+# RCPbind
+iptables -A INPUT -s 192.168.152.20 -p tcp --destination-port 111 -j ACCEPT
+iptables -A INPUT -s 192.168.152.171 -p tcp --destination-port 111 -j ACCEPT
+iptables -A INPUT -s 192.168.176.63 -p tcp --destination-port 111 -j ACCEPT
+iptables -A INPUT -s 192.168.186.70 -p tcp --destination-port 111 -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 111 -j REJECT
+
